556e4c9f043f2e480a13b6e37a9603d998fa8b86
Josh Ellis <joshe523@gmail.com>
2020-08-03 13:43:29 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,26 @@
-import React, { Component } from 'react';
-import './App.css';
-import { connect } from 'react-redux';
-import { addItem } from  './actions/items';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import './App.css'
 
 class App extends Component {
-
-  handleOnClick() {
-    this.props.store.dispatch(addItem());
+  handleOnClick () {
+    this.props.addItem() // Code change: this.props.store.dispatch is no longer being called
   }
 
-  render() {
+  render () {
     return (
-      <div className="App">
-        <button onClick={(event) => this.handleOnClick(event)}>
-          Click
-          </button>
+      <div className='App'>
+        <button onClick={event => this.handleOnClick(event)}>Click</button>
         <p>{this.props.items.length}</p>
       </div>
-    );
+    )
   }
-};
+}
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     items: state.items
-  };
-};
+  }
+}
 
-export default connect(mapStateToProps)(App);
+export default connect(mapStateToProps, { addItem })(App) // Code change: no mapDispatchToProps function required!
