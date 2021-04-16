38a37641d73cba6d5724abf90cb7caf2e38e50c1
Robert Clarke <r.jason.clarke@gmail.com>
2021-04-16 15:42:21 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './index.css';
 import { Provider } from 'react-redux';
 //
 const store = createStore(shoppingListItemReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-
+//
 ReactDOM.render(
   <Provider store={store}>
     <App store={store}/>
