e614384adf1e8f97b9f3a7f0cfb2038a21256f42
yarvinh <yarvinhhernandez@gmail.com>
2021-06-22 22:19:38 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -6,10 +6,13 @@ import { addItem } from  './actions/items';
 class App extends Component {
 
   handleOnClick() {
-    this.props.store.dispatch(addItem());
+    // console.log("from handleOnClick",this.props.store)
+    this.props.addItem() 
+    // this.props.store.dispatch(addItem());
   }
 
   render() {
+  
     return (
       <div className="App">
         <button onClick={(event) => this.handleOnClick(event)}>
@@ -21,10 +24,19 @@ class App extends Component {
   }
 };
 
-const mapStateToProps = (state) => {
-  return {
-    items: state.items
-  };
-};
+// const mapStateToProps = (state) => {
+//   return {
+//     items: state.items
+//   };
+// };
 
-export default connect(mapStateToProps)(App);
+
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     addItem:() => {
+//       dispatch(addItem())
+//     }
+//   };
+// };
+
+export default connect(state => ({ items: state.items }),{ addItem })(App);
